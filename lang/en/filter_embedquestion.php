e03a58c745b49478a1226cfa0b814562d2c2c67d
danghieu1407 <hnd34@open.ac.uk>
2025-01-20 11:15:14 +0700

    Embedded questions\Student feedback: reminder about summary page #847349

--- lang/en/filter_embedquestion.php
+++ lang/en/filter_embedquestion.php
@@ -67,6 +67,7 @@ $string['nameidnumberandcount'] = '{$a->name} [{$a->idnumber}] ({$a->count})';
 $string['noguests'] = 'Guest users do not have permission to interact with embedded questions.';
 $string['notyourattempt'] = 'This is not your attempt.';
 $string['pluginname'] = 'Embed questions';
+$string['previousattempts'] = 'Previous attempts';
 $string['privacy:metadata'] = 'The Embed questions filter does not store any personal data.';
 $string['questionbank'] = 'Question bank';
 $string['questionidnumber'] = 'Question id number';
